e165de8b1fbcdd25952a72f884d5a988914a0d48
srikanthdhondi <srikanth@hiwaldo.com>
2025-01-06 17:46:45 +0530

    Assignment updates
    Assignment updates

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="row">
+      <BoxComponent
+        v-for="id in [1, 2, 3]"
+        :key="id"
+        :id="id"
+        :isActive="activeBoxes.includes(id)"
+        :isFadingOut="fadingBoxes.includes(id)"
+        @box-clicked="onBoxClick"
+      />
+    </div>
+    <div class="row">
+      <BoxComponent
+        v-for="id in [4, 5]"
+        :key="id"
+        :id="id"
+        :isActive="activeBoxes.includes(id)"
+        :isFadingOut="fadingBoxes.includes(id)"
+        @box-clicked="onBoxClick"
+      />
+    </div>
+    <div class="row">
+      <BoxComponent
+        v-for="id in [6, 7, 8]"
+        :key="id"
+        :id="id"
+        :isActive="activeBoxes.includes(id)"
+        :isFadingOut="fadingBoxes.includes(id)"
+        @box-clicked="onBoxClick"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoxComponent from "./BoxComponent.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    BoxComponent,
+  },
+  data() {
+    return {
+      activeBoxes: [],
+      fadingBoxes: [],
+    };
+  },
+  methods: {
+    onBoxClick(id) {
+      if (id === 8) {
+        this.triggerFadeOut();
+      } else {
+        this.toggleBox(id);
+      }
+    },
+    toggleBox(id) {
+      if (!this.activeBoxes.includes(id)) {
+        this.activeBoxes.push(id);
+      }
+    },
+    async triggerFadeOut() {
+      while (this.activeBoxes.length) {
+        const lastBox = this.activeBoxes.pop();
+        this.fadingBoxes.push(lastBox);
+
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        this.fadingBoxes = this.fadingBoxes.filter(
+          (boxId) => boxId !== lastBox
+        );
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.row {
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
 }
 </style>
---
+++ src/components/BoxComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="box"
+    :class="{ fadeOut: isFadingOut }"
+    :style="{ backgroundColor: isActive ? 'green' : 'white' }"
+    @click="handleClick"
+  >
+    <span>{{ id }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    isActive: Boolean,
+    isFadingOut: Boolean,
+  },
+  emits: ["box-clicked"],
+  methods: {
+    handleClick() {
+      this.$emit("box-clicked", this.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  font-weight: bold;
+  margin: 5px;
+  cursor: pointer;
+  user-select: none;
+  transition: opacity 0.5s ease-in-out, background-color 0.5s ease-in-out;
+}
+
+.fadeOut {
+  opacity: 0;
+}
+</style>
